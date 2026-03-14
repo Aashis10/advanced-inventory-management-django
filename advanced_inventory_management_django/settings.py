6d57e5105ae359bab297e8119c8d5812510bca4c
Aashis10 <aashishbhusal50@gmail.com>
2026-03-14 08:37:28 +0545

    Fix DisallowedHost by adding Render URL to ALLOWED_HOSTS

--- advanced_inventory_management_django/settings.py
+++ advanced_inventory_management_django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5d%kehnxd7nz8dn(dc#m++=jxn7&d8ev%%z$m-)oq1mp)a-x5i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["advanced-inventory-management-django.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
